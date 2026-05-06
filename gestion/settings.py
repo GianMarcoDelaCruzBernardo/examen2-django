fd15adf22592b1e147a4848f32e49d708bb43960
Deploy Fix <fix@deploy.com>
2026-05-06 22:13:37 +0000

    fix: cloudinary_storage antes de staticfiles en INSTALLED_APPS

--- gestion/settings.py
+++ gestion/settings.py
@@ -17,9 +17,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'cloudinary_storage',
     'cloudinary',
+    'django.contrib.staticfiles',
     'proyectos',
 ]
 
